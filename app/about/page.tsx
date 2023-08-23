fe12c45b4d551bf066454ab02c0769f407d9ee7a
sunyoung <sunyoungah69@gmail.com>
2023-08-23 10:23:15 +0900

    feature / about page 생성

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+const Page = () => {
+	return (
+		<div>
+			<p>
+				<b>About page</b>
+			</p>
+			{/* 
+				기본 라우팅
+				about이라는 폴더 아래에 page.tsx라는 파일을 만들어서
+				'http://localhost:3000/about' 경로에서 페이지 확인 가능
+			*/}
+		</div>
+	);
+};
+
+export default Page;
