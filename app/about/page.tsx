ebff079541355efcead7a96799dad37c659402d2
sunyoung <sunyoungah69@gmail.com>
2023-08-23 11:03:07 +0900

    feature / 페이지 간 navigating

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,17 @@
+import Link from 'next/link';
+
 const Page = () => {
 	return (
 		<div>
-			<p>
-				<b>About page</b>
-			</p>
+			<h1>About page</h1>
 			{/* 
 				기본 라우팅
 				about이라는 폴더 아래에 page.tsx라는 파일을 만들어서
 				'http://localhost:3000/about' 경로에서 페이지 확인 가능
 			*/}
+
+			<Link href="/">Return Home</Link>
+			<Link href="/about/company">Company</Link>
 		</div>
 	);
 };
