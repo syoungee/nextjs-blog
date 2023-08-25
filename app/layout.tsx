8f7e3b1bfa5f229ea1334cbd0ba675ee48f374f8
sunyoung <sunyoungah69@gmail.com>
2023-08-25 15:47:20 +0900

    chore / layout padding top 10

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en">
 			<body className={inter.className}>
 				<Navigation />
-				{children}
+				<div className="pt-10">{children}</div>
 				<Footer />
 			</body>
 		</html>
