f099f125438b16ab1b1b6a859e5d91a16cbe71f9
syoungee <liahwang@tecace.com>
2023-08-27 12:31:00 +0900

    feature / post content component

--- components/post/post-card.tsx
+++ components/post/post-card.tsx
@@ -1,18 +1,19 @@
 import { Post } from '@/types/collection';
 import Link from 'next/link';
 import Image from 'next/image';
+import PostContent from './post-content';
 
 interface PostProps {
-	post: Post;
+  post: Post;
 }
 const PostCard = ({ post }: PostProps) => {
-	return (
-		<Link className="grid grid-cols-2 gap-10" href={`/post/${post.slug}`}>
-			{/* Post Image */}
-			<Image alt={post.title} src={post.image} width={600} height={300} />
-			<div>{post.title}</div>
-		</Link>
-	);
+  return (
+    <Link className="grid grid-cols-2 gap-10" href={`/post/${post.slug}`}>
+      {/* Post Image */}
+      <Image className="rounded-md w-full object-cover object-center max-h-[300px]" alt={post.title} src={post.image} width={600} height={300} />
+      <PostContent post={post} />
+    </Link>
+  );
 };
 
 export default PostCard;
---
+++ components/post/post-content.tsx
@@ -0,0 +1,35 @@
+import { Post } from '@/types/collection';
+import { ArrowRight } from 'lucide-react';
+
+interface PostContentProps {
+  post: Post;
+}
+const PostContent = ({ post }: PostContentProps) => {
+  return (
+    <div className="space-y-2">
+      {/* Tags */}
+      <div className={`flex items-center flex-wrap gap-2  text-neutral-400 text-sm }`}>
+        <div className={`font-medium ${post.category.title === 'Cities' ? 'text-emerald-600' : 'text-indigo-600'}`}>{post.category.title}</div>
+        <div className="w-2 h-2 rounded-full bg-neutral-200" />
+        <div>{`${post.author.first_name} ${post.author.last_name}`}</div>
+        <div className="w-2 h-2 rounded-full bg-neutral-200" />
+        <div>1 min read</div>
+        <div className="w-2 h-2 rounded-full bg-neutral-200" />
+        <div>1 month ago</div>
+      </div>
+      {/* Title */}
+      <h2 className={'text-2xl font-medium'}>{post.title}</h2>
+      {/* Description */}
+      <p className="leading-snug text-neutral-600">{post.description}</p>
+      {/* Read More */}
+      {
+        <div className="flex items-center gap-2 pt-3">
+          Read More
+          <ArrowRight size="14" />
+        </div>
+      }
+    </div>
+  );
+};
+
+export default PostContent;
