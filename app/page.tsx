312631dd6826640125ec857c565ec4bef7ab4e8b
syoungee <liahwang@tecace.com>
2023-08-27 16:55:53 +0900

    feature / post list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import PaddingContainer from '@/components/layout/padding-container';
 import PostCard from '@/components/post/post-card';
+import PostList from '@/components/post/post-list';
 import { DUMMY_POSTS } from '@/DUMMY_DATA';
 
 export default function Home() {
-	return (
-		<PaddingContainer>
-			<main className="h-auto space-y-10">
-				<PostCard post={DUMMY_POSTS[0]} />
-			</main>
-		</PaddingContainer>
-	);
+  return (
+    <PaddingContainer>
+      <main className="h-auto space-y-10">
+        <PostCard post={DUMMY_POSTS[0]} />
+        <PostList layout="vertical" posts={DUMMY_POSTS.filter((post, index) => index > 0 && index < 3)} />
+      </main>
+    </PaddingContainer>
+  );
 }
--- components/post/post-card.tsx
+++ components/post/post-card.tsx
@@ -5,10 +5,11 @@ import PostContent from './post-content';
 
 interface PostProps {
   post: Post;
+  layout?: 'vertical' | 'horizontal';
 }
-const PostCard = ({ post }: PostProps) => {
+const PostCard = ({ post, layout = 'horizontal' }: PostProps) => {
   return (
-    <Link className="grid grid-cols-2 gap-10" href={`/post/${post.slug}`}>
+    <Link className={`${layout === 'horizontal' ? 'grid grid-cols-2 gap-10' : 'space-y-10'}`} href={`/post/${post.slug}`}>
       {/* Post Image */}
       <Image className="rounded-md w-full object-cover object-center max-h-[300px]" alt={post.title} src={post.image} width={600} height={300} />
       <PostContent post={post} />
