78c26f1c24774c542925490752f396594611a3c6
sunyoung <sunyoungah69@gmail.com>
2023-08-28 14:52:16 +0900

    feature / cta card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import PaddingContainer from '@/components/layout/padding-container';
 import PostCard from '@/components/post/post-card';
 import PostList from '@/components/post/post-list';
+import CTACard from '@/components/elements/cta-card';
 import { DUMMY_POSTS } from '@/DUMMY_DATA';
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
 			<main className="h-auto space-y-10">
 				<PostCard post={DUMMY_POSTS[0]} />
 				<PostList layout="vertical" posts={DUMMY_POSTS.filter((_post, index) => index > 0 && index < 3)} />
+				<CTACard />
 				<PostCard reverse post={DUMMY_POSTS[3]} />
 				<PostList posts={DUMMY_POSTS.filter((_post, index) => index > 3 && index < 6)} />
 			</main>
