b0bb42f1afcda114dd411eba38b55a3dcce09066
sunyoung <sunyoungah69@gmail.com>
2023-08-30 15:07:02 +0900

    feature / env file

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { notFound } from 'next/navigation';
 import PaddingContainer from '@/components/layout/padding-container';
 import PostHero from '@/components/post/post-hero';
 import SocialLink from '@/components/elements/social-link';
+import PostBody from '@/components/post/post-body';
+import CTACard from '@/components/elements/cta-card';
 
 export const generateStaticParams = async () => {
 	return DUMMY_POSTS.map((post) => {
@@ -21,29 +23,34 @@ const Page = ({ params }: { params: { slug: string } }) => {
 
 	return (
 		<PaddingContainer>
-			<PostHero post={post}></PostHero>
-			<div className="flex gap-10 mt-10">
-				<div className="relative">
-					<div className="sticky flex flex-col gap-5 top-20">
-						<SocialLink
-							platform="facebook"
-							isShareURL
-							link={`https://www.facebook.com/sharer.php?u=${'http://localhost:3000' + `/post/${post.slug}`}`}
-						></SocialLink>
-						<SocialLink
-							platform="twitter"
-							isShareURL
-							link={`https://www.facebook.com/sharer.php?u=${'http://localhost:3000' + `/post/${post.slug}`}`}
-						></SocialLink>
-						<SocialLink
-							platform="linkedin"
-							isShareURL
-							link={`https://www.facebook.com/sharer.php?u=${'http://localhost:3000' + `/post/${post.slug}`}`}
-						></SocialLink>
+			<div>
+				{/* Post Hero */}
+				<PostHero post={post}></PostHero>
+				{/* Post Body and Social Share */}
+				<div className="flex flex-col gap-10 md:flex-row">
+					<div className="relative">
+						<div className="sticky flex md:flex-col gap-5 top-20">
+							<SocialLink
+								platform="facebook"
+								isShareURL
+								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`}
+							></SocialLink>
+							<SocialLink
+								platform="twitter"
+								isShareURL
+								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`}
+							></SocialLink>
+							<SocialLink
+								platform="linkedin"
+								isShareURL
+								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`}
+							></SocialLink>
+						</div>
 					</div>
+					<PostBody body={post.body}></PostBody>
 				</div>
-
-				<div className="h-[1200px] bg-slate-200 w-full">Post Body</div>
+				{/* CTA Card */}
+				<CTACard />
 			</div>
 		</PaddingContainer>
 	);
