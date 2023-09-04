3fc36df5f73d384e711e8e387eada0ad0bc8ef8d
sunyoung <sunyoungah69@gmail.com>
2023-09-04 16:59:02 +0900

    feature / change nextjs config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
 				hostname: 'images.unsplash.com',
 				protocol: 'https',
 			},
+			{
+				hostname: 'directus-production-a4cd.up.railway.app',
+				protocol: 'https',
+			},
+			{
+				hostname: 'directus-production-a4cd.up.railway.app',
+				protocol: 'http',
+			},
 		],
 	},
 };
