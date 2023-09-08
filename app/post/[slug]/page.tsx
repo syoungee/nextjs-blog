72f8407d613a35181dee4ccceae78ac18933e9e0
sunyoung <sunyoungah69@gmail.com>
2023-09-08 15:25:03 +0900

    feature / filter post data

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -5,19 +5,55 @@ import PostHero from '@/components/post/post-hero';
 import SocialLink from '@/components/elements/social-link';
 import PostBody from '@/components/post/post-body';
 import CTACard from '@/components/elements/cta-card';
+import { directus } from '@/lib/directus';
+import { readItems } from '@directus/sdk';
 
 export const generateStaticParams = async () => {
-	return DUMMY_POSTS.map((post) => {
-		return {
-			slug: post.slug,
-		};
-	});
+	// return DUMMY_POSTS.map((post) => {
+	// 	return {
+	// 		slug: post.slug,
+	// 	};
+	// });
+
+	try {
+		const posts = await directus.request(
+			readItems('post', {
+				fields: ['slug'],
+			})
+		);
+		const params = posts?.map((post) => {
+			return { slug: post.slug as string };
+		});
+		// console.log('params?', params);
+		return params || [];
+	} catch (err) {
+		console.log(err);
+		throw new Error('Error fetching posts');
+	}
 };
 
-const Page = ({ params }: { params: { slug: string } }) => {
-	const post = DUMMY_POSTS.find((post) => post.slug === params.slug);
+const Page = async ({ params }: { params: { slug: string } }) => {
+	// const post = DUMMY_POSTS.find((post) => post.slug === params.slug);
 
-	if (!post) {
+	const getPostData = async () => {
+		const post = await directus.request(
+			readItems('post', {
+				fields: ['*'],
+			})
+		);
+		return post;
+	};
+
+	const posts = await getPostData();
+	// console.log(posts[0].slug);
+	// console.log('page params?', params);
+	const post = posts.filter((post) => {
+		// console.log('post index?', post.slug, params.slug);
+		return post.slug == params.slug;
+	});
+	// console.log(post[0]);
+
+	if (!posts) {
 		notFound();
 	}
 
@@ -25,7 +61,7 @@ const Page = ({ params }: { params: { slug: string } }) => {
 		<PaddingContainer>
 			<div className="space-y-10">
 				{/* Post Hero */}
-				<PostHero post={post}></PostHero>
+				<PostHero post={post?.[0]}></PostHero>
 				{/* Post Body and Social Share */}
 				<div className="flex flex-col gap-10 md:flex-row">
 					<div className="relative">
@@ -34,21 +70,21 @@ const Page = ({ params }: { params: { slug: string } }) => {
 							<SocialLink
 								platform="facebook"
 								isShareURL
-								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`}
+								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post[0].slug}`}`}
 							></SocialLink>
 							<SocialLink
 								platform="twitter"
 								isShareURL
-								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`}
+								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post[0].slug}`}`}
 							></SocialLink>
 							<SocialLink
 								platform="linkedin"
 								isShareURL
-								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post.slug}`}`}
+								link={`https://www.facebook.com/sharer.php?u=${`${process.env.NEXT_PUBLIC_SITE_URL}/post/${post[0].slug}`}`}
 							></SocialLink>
 						</div>
 					</div>
-					<PostBody body={post.body}></PostBody>
+					<PostBody body={posts[0].body}></PostBody>
 				</div>
 				{/* CTA Card */}
 				<CTACard />
