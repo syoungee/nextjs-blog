ed49ff81281984c15d713edfb7a356bbabf455a3
sunyoung <sunyoungah69@gmail.com>
2023-09-08 16:55:03 +0900

    feature / post-body

--- components/post/post-body.tsx
+++ components/post/post-body.tsx
@@ -1,11 +1,31 @@
 import parse from 'html-react-parser';
+import Image from 'next/image';
+// parsing images as next image
 
 const PostBody = ({ body }: { body: string }) => {
 	const getParsedHTML = (body: string) => {
 		return parse(body);
 	};
+	const options = {
+		replace: (domNode) => {
+			if (domNode instanceof Element && domNode.attribs) {
+				if (domNode.name === 'img') {
+					const { src, alt } = domNode.attribs;
+					return (
+						<Image
+							className="rounded-md w-full object-cover object-center my-3 h-auto max-h-[300px] md:max-h-[500px]"
+							src={src}
+							alt={alt}
+							width={1200}
+							height={620}
+						></Image>
+					);
+				}
+			}
+		},
+	};
 
-	return <div>{getParsedHTML(body)}</div>;
+	return <div className="rich-text">{getParsedHTML(body)}</div>;
 };
 
 export default PostBody;
