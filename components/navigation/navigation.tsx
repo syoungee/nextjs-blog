9c383340b7c25db62e9f9eb866a8e66c822c5e11
sunyoung <sunyoungah69@gmail.com>
2023-09-14 14:50:44 +0900

    feature / navigation with locale data

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,22 +1,25 @@
 import Link from 'next/link';
 import PaddingContainer from '../layout/padding-container';
+import { getDictionary } from '@/lib/getDictionary';
 
-const Navigation = () => {
+const Navigation = async ({ locale }: { locale: string }) => {
+	const dictionary = await getDictionary(locale);
+
 	return (
 		<div className="sticky top-0 left-0 right-0 z-[999] bg-white border-b bg-opacity-70 backdrop-blur-md">
 			<PaddingContainer>
 				<div className="flex items-center justify-between py-5">
-					<Link className="text-lg font-bold" href="/">
+					<Link className="text-lg font-bold" href="/en">
 						Explorer
 					</Link>
 					{/* Category Links */}
 					<nav>
 						<ul className="flex items-center gap-4 text-neutral-600">
 							<li>
-								<Link href="/cities">Cities</Link>
+								<Link href="/cities">{dictionary.navigation.links.cities}</Link>
 							</li>
 							<li>
-								<Link href="/experiences">Experiences</Link>
+								<Link href="/experiences">{dictionary.navigation.links.experience}</Link>
 							</li>
 						</ul>
 					</nav>
